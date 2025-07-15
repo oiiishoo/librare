3fb1045231355220fc897982ea03060ce3049c00
oiiishoo <70274902+oiiishoo@users.noreply.github.com>
2025-07-15 08:57:55 +0400

    Update trie.h

--- trie.h
+++ trie.h
@@ -1,34 +1,69 @@
 #pragma once
-//adaptive school level bigdata searching algorithm
-enum TrieMode {
-	STATIC= 1 << 13,
-	DYNAMIC= 1 << 14,
-	ITERATIVE=1<<15
+#include<iostream>
+#include<list>
+
+namespace Utrie{
+	void stnod_reset() {
+
+	}
+	void stnod_set(char elem_idx,char*ptr) {
+
+	}
 };
-struct targpack {
-	bool st, dm, it;
-	short bits;
-	targpack(short args):st(0),dm(0),it(0) {
-		short bits = 0;
-		if (STATIC & args)st=1;
-		if (DYNAMIC & args)dm = 1;
-		if (ITERATIVE & args)it=1;
-		if (st) bits |= STATIC;
-		if (dm) bits |= DYNAMIC;
-		if (it) bits |= ITERATIVE;
+
+//template<typename T>
+class tnod {
+public:
+	//alligned typesafe
+	char hasval : 1, typ : 1;
+	//opted if key
+	char key;
+	char unused[6];
+	//external struct
+	tnod(char _hasval,char _type,char _key,size_t _unus6b) {
+		hasval = _hasval;
+		typ = _type;
+		key = _key;
+		*(size_t*)unused = ((~(65535<<47))&_unus6b);
 	}
 };
 template<typename T>
 class Trie {
-	targpack args;
-	Trie(short args_):args(args_) {
+	char lay[256];
+
+
+public:
+	size_t size;
+	short uniq_symbols;
+	//делать через хранение указателей
+
+	Trie(const char*allowed_symbols) {
+		char uniquesymbols[256];
+		memset(uniquesymbols,0,256);
+		memset(lay, 0, 256);
+		for (size_t i = 0; i < 256; i++)
+		{
+			if (allowed_symbols[i] == 0)break;
+			uniquesymbols[allowed_symbols[i]]=1;
+			
+		}
 		
+		int temp=0;
+		for (size_t i = 0; i < 256; i++)
+		{
+			if (uniquesymbols[i]) {
+				lay[i] = temp;
+				temp++;
+			}
+		}
 	}
 	Trie() {
-		
+		dict.reserve(256);
+		d.cks;
 	}
-
+	//linear search
 	bool has(T&buf) {
 
 	}
+	//поиск снизу
 };
