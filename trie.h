66c8cd0969822443c17fce2fa494bd27fe75b00e
oiiishoo <70274902+oiiishoo@users.noreply.github.com>
2025-07-30 03:36:56 +0400

    Update trie.h

--- trie.h
+++ trie.h
@@ -1,55 +1,155 @@
 #pragma once
-#include<iostream>
 #include<list>
-
-namespace Utrie{
-	void stnod_reset() {
-
-	}
-	void stnod_set(char elem_idx,char*ptr) {
-
-	}
-};
+#include"libs.hpp"
+	
 
 //template<typename T>
+//hasval
+//typ
 class tnod {
 public:
 	//alligned typesafe
 	char hasval : 1, typ : 1;
-	//opted if key
-	char key;
-	char unused[6];
+	char unused[7];
 	//external struct
-	tnod(char _hasval,char _type,char _key,size_t _unus6b) {
+	tnod(char _hasval, char _type
+		//, szt _unus7b
+	) {
 		hasval = _hasval;
 		typ = _type;
-		key = _key;
-		*(size_t*)unused = ((~(65535<<47))&_unus6b);
+		//*(szt*)unused = ((~(255 << 55)) & _unus7b);
+	}
+	tnod() {
+
 	}
 };
 template<typename T>
 class Trie {
 	char lay[256];
+	char* darray;
+	std::list<char*> heaps;
+	/*iterative
+	struct tnod
+	int items
+	val
+	keys first
+	pointers * current keys
+	*/
+	str ImakeAbstruct(int items) {
+		if ((!items) || (items > 256))return 0;
+		int elementsMem = (sizeof(str) + 1) * items;
+		str res = strc malloc(elementsMem + sizeof(tnod) + 4+sizeof(T));
+		if (!res)return 0;
+		tnod t(0, 0);
+		*(tnod*)(res) = t;
+		*((int*)res + sizeof(tnod)) = 0;
+		return res;
+	}
+	void Iset(char key, str iterativeNode, str value) {
+		if (!iterativeNode)return;
 
+		*((szt*)iterativeNode + sizeof(tnod)) += 1;
+	}
+	void Ifree(char key, str iterativeNode) {
+		if (!iterativeNode)return;
+
+	}
+	/*static no keys / uses lay/offset layout
+	struct tnod
+	int items
+	val
+	raw pointers * items count
+	*/
+	str SmakeAbstruct(int items) {
+		if ((!items) || (items > 256))return 0;
+		int elementsMem = sizeof(str) * items;
+		str res = strc malloc(elementsMem + sizeof(tnod) + 4);
+		if (!res)return 0;
+		tnod t(0, 1);
+		t.
+		*(tnod*)(res) = t;
+		*((int*)staticnode) = items;
+		str core = strc(res + 4 + sizeof(tnod)+sizeof(T));
+		for (size_t i = 0; i < items; i++)
+		{
+			core[i] = 0;
+		}
+		heaps.emplace_back(res);
+		return res;
+	}
+	void SsetVal(str staticnode,T val) {
+		if (!staticnode)return;
+		*(tnod*)(staticnode+4+ sizeof(tnod)) = val;
+	}
+	void SsetPTR(int offset, str staticnode, str ptr) {
+		if ((offset < 0) || (offset > 255) || (!staticnode))return;
+		if (offset > (*((int*)(staticnode+sizeof(tnod)))-1))return;
+		( (str*)(staticnode + 4 + sizeof(tnod)+sizeof(T)) )[offset] = ptr;
+	}
+	bool Shasval(str staticnode,T*buffer=0) {
+		if (!staticnode)return;
+		tnod t= *(tnod*)staticnode;
+		if (t.hasval) {
+			if (buffer) {
+				*buffer = t;
+			}
+			return 1;
+		}
+		return 0;
+	}
+	void Sfree(str staticnode) {
+		if (!staticnode)return;
+		int items=*(int*)(staticnode+sizeof(tnod));
+		if (!items) {
+			free(staticnode);
+			return;
+		}
+		str core=strc(staticnode+4+sizeof(tnod)+sizeof(T));
+		//todo recursive 
+		for (size_t i = 0; i < items; i++)
+		{
+			if (core[i]) {
+				*(int*)(core + sizeof(tnod))
+				if()
+				free(core[i]);
+			}
+		}
+	}
 
 public:
 	size_t size;
-	short uniq_symbols;
+	int uniq_symbols;
+	unsigned short maxLength;
 	//делать через хранение указателей
 
-	Trie(const char*allowed_symbols) {
-		char uniquesymbols[256];
-		memset(uniquesymbols,0,256);
-		memset(lay, 0, 256);
-		for (size_t i = 0; i < 256; i++)
+	Trie(str allowed_symbols,short treelength) {
+		maxLength = treelength;
+		const int evil=256;
+		for( char* _:heaps)
 		{
-			if (allowed_symbols[i] == 0)break;
+			free(_);
+		}
+		if (!allowed_symbols) {
+			//binary fill
+			for (szt i = 0; i < evil; i++)
+			{
+				lay[i] = i;
+
+			}
+			return;
+		}
+		char uniquesymbols[evil];
+		memset(uniquesymbols,0, evil);
+		memset(lay, 0, evil);
+
+		for (szt i = 0; i < evil; i++)
+		{
+			if (!allowed_symbols[i])break;
 			uniquesymbols[allowed_symbols[i]]=1;
-			
 		}
 		
 		int temp=0;
-		for (size_t i = 0; i < 256; i++)
+		for (szt i = 0; i < evil; i++)
 		{
 			if (uniquesymbols[i]) {
 				lay[i] = temp;
@@ -57,13 +157,36 @@ public:
 			}
 		}
 	}
+	Trie(cstr allowed_symbols) {
+		Trie(strc allowed_symbols);
+	}
+	//Empty initializer
 	Trie() {
-		dict.reserve(256);
-		d.cks;
+		
 	}
 	//linear search
-	bool has(T&buf) {
+	bool has(T*buf) {
 
 	}
-	//поиск снизу
+	T& operator[](cstr _cstr) {
+		findBykey(_cstr);
+		//return
+	}
+	T& operator[](str _str) {
+		findBykey(_str);
+		//return 
+	}
+
+	bool findBykey(str _str) {
+		if (_str) {
+			
+		}
+		return 0;
+	}
+
+	bool findBykey(cstr _cstr) {
+
+		return findBykey(strc _cstr);
+	}
+
 };
