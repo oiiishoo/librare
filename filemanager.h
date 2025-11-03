7a790a9529cabde3005003193f493ad6b1288dd8
oiiishoo <70274902+oiiishoo@users.noreply.github.com>
2025-11-03 09:57:09 +0400

    Add filemanager.h with example usage comments

---
+++ filemanager.h
@@ -0,0 +1,16 @@
+//in executable .cpp
+/*#include"filemanager.h"
+int main() {
+    while (1) {
+        FILETIME FT;
+
+        FiletimeFromLocal(&FT);
+        std::cout << *(size_t*)&FT<<' ';
+        FiletimeFromTimestamp(&FT);
+        std::cout << *(size_t*)&FT<<'\n';
+        Sleep(1000);
+    }
+}*/
+
+
+
